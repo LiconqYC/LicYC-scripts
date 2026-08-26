49f9ac86c81a8282ec8ce15fa743453f6f5eb462
LiconqYC <pinkodmmm@gmail.com>
2026-08-26 12:37:22 +0300

    Update vape-ui-by-dawid-v2.lua

--- vape-ui-by-dawid-v2.lua
+++ vape-ui-by-dawid-v2.lua
@@ -121,7 +121,7 @@ function lib:Window(text, preset, closebind)
     TabHold.Name = "TabHold"
     TabHold.Parent = Main
     TabHold.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
-    TabHold.BackgroundTransparency = 0.000
+    TabHold.BackgroundTransparency = 1.000
     TabHold.Position = UDim2.new(0.0339285731, 0, 0.147335425, 0)
     TabHold.Size = UDim2.new(0, 107, 0, 254)   -- можно оставить ту же высоту, но теперь будет скролл
     TabHold.Active = true                       -- разрешаем взаимодействие
